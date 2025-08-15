849c9b5d6aa595debb1a5682aac9f3dda69fec9d
R <R@DESKTOP-MMP9RH3>
2025-08-15 05:13:59 +0300

    finish

---
+++ اشارات المرور/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace TrafficLight
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        { 
+            
+            
+            while (true) // تكرار مستمر
+            {
+                // أحمر
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                stars();
+                Console.ResetColor();
+                Thread.Sleep(2000); // انتظر 2 ثانية
+
+                // أصفر
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                stars();
+                Console.ResetColor();
+                Thread.Sleep(1000); // انتظر ثانية
+
+                // أخضر
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                stars();
+                Console.ResetColor();
+                Thread.Sleep(2000); // انتظر 2 ثانية
+            }
+        }
+        public static void stars()
+        {
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i<=5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int j = 0; j<=5; j++)
+                {
+                    Console.Write(" * ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
